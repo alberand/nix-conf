08b925b8aa1f6b967d7dad6dec6d3136b4b2ef4f
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2023-02-08 10:23:21 +0100

    neovim: add some neat remaps from theprimagen

--- configs/neovim/init.lua
+++ configs/neovim/init.lua
@@ -1,8 +1,36 @@
 vim.cmd [[source /nix/store/yiqb3gmbmcgx2vhghsabcbb3qbcrwi8d-nvim-init-home-manager.vim]]
 
+-- Fat cursor
+vim.opt.guicursor = ""
+-- Error column always on
 vim.opt.signcolumn = "yes"
+-- Line numbering
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.smartindent = true
+
+vim.opt.incsearch = true
 
 -- Mappings.
+-- Move blocks with JK
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Smarter J line add
+vim.keymap.set("n", "J", "mzJ`z")
+-- Half page jumps are in the middle of the screen
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Search terms are in the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Copy to clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
