a75a563be4340854e53c391c646a0315fa41146b
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2023-04-19 20:10:18 +0200

    Fix wofi width (alt-p)

--- modules/wofi.nix
+++ modules/wofi.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   xdg.configFile."wofi/config".text = ''
-                width=200
+      width=800
   '';
 
   xdg.configFile."wofi/style.css".source = ../configs/wofi-theme.css;
