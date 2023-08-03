f0d396f709dda4ba2332aed65116d47a3126908f
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2023-08-03 13:14:51 +0200

    thinky: change user to aalbersh

--- machines/thinky/configuration.nix
+++ machines/thinky/configuration.nix
@@ -44,7 +44,7 @@
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.alberand = {
+  users.users.aalbersh = {
     isNormalUser = true;
     description = "Andrey Albershteyn";
     uid = 1000;
@@ -54,7 +54,6 @@
       "sudo"
       "libvirt"
       "networkmanager"
-      "wireshark"
       "disk"
     ];
   };
