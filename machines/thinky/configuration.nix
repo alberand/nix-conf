9b3e72381d4c18e75c03298356cb20794c25a702
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2023-09-07 13:37:06 +0200

    thinky: gateway causes error (not sure why)

--- machines/thinky/configuration.nix
+++ machines/thinky/configuration.nix
@@ -46,7 +46,7 @@
     # Pick only one of the below networking options.
     networkmanager.enable = true;
     networkmanager.dns = "default";
-    defaultGateway = "192.168.0.1";
+    #defaultGateway = "192.168.0.1";
     # nameservers = [ "8.8.8.8" "1.1.1.1" ];
     interfaces.enp0s20f0u3.useDHCP = true;
     # VPN configuration
