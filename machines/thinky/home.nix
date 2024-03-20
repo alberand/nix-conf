2ed7b2528cdaa29ed90785cc57dbd800d69e35df
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2024-03-20 12:21:03 +0100

    thinky: just comments

--- machines/thinky/home.nix
+++ machines/thinky/home.nix
@@ -14,6 +14,7 @@
     rust-analyzer
     pinentry
     conserver
+    # Script to open serial console to Beaker machine
     (let
         wrapper = (writeShellScriptBin "con" (builtins.readFile ./configs/console.sh));
     in pkgs.symlinkJoin {
@@ -23,7 +24,9 @@
         wrapper
       ];
     })
+    # Git script to backport fixes from upstream to downstream
     (writeShellScriptBin "git-bp" (builtins.readFile ./configs/git-bp))
+    # Beaker script to reserve machines for testing
     (writeShellScriptBin "machine" (builtins.readFile ./configs/machine))
   ];
 
@@ -54,13 +57,15 @@
     enable = true;
   };
 
-  # Script to check work VPN connectivity
+  # Waybar widget to show work VPN connectivity
   programs.waybar.settings.mainBar."custom/vpn".exec =
     (builtins.readFile ./configs/vpn-check.sh);
 
+  # Waybar widget to show SSH and Kerberos state
   programs.waybar.settings.mainBar."custom/access".exec =
     (pkgs.writeShellScriptBin "access" (builtins.readFile ./configs/access.sh)) + "/bin/access";
 
+  # Auto-run applications
   wayland.windowManager.sway.config = {
     startup = [
       { command = "swaymsg 'workspace 1; exec firefox'"; }
@@ -69,6 +74,7 @@
     ];
   };
 
+  # Waybar widgets position and battery
   programs.waybar.settings.mainBar = {
     modules-right = lib.mkForce [
       "custom/access"
