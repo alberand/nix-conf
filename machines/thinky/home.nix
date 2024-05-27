d900c655609ce88e6ec885cfc8867e4cd92681fe
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2024-05-27 16:24:29 +0200

    thinky: waybar configuration cleaning + thinky-home formatting

--- machines/thinky/home.nix
+++ machines/thinky/home.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, lib, ... }: {
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [
     ../../modules/home-base.nix
     ../../modules/neomutt.nix
@@ -17,14 +22,15 @@
     rpm
     # Script to open serial console to Beaker machine
     (let
-        wrapper = (writeShellScriptBin "con" (builtins.readFile ./configs/console.sh));
-    in pkgs.symlinkJoin {
-      name = "conserver";
-      paths = [
-        conserver
-        wrapper
-      ];
-    })
+      wrapper = writeShellScriptBin "con" (builtins.readFile ./configs/console.sh);
+    in
+      pkgs.symlinkJoin {
+        name = "conserver";
+        paths = [
+          conserver
+          wrapper
+        ];
+      })
     # Git script to backport fixes from upstream to downstream
     (writeShellScriptBin "git-bp" (builtins.readFile ./configs/git-bp))
     # Beaker script to reserve machines for testing
@@ -58,48 +64,50 @@
     enable = true;
   };
 
-  # Waybar widget to show work VPN connectivity
-  programs.waybar.settings.mainBar."custom/vpn".exec =
-    (builtins.readFile ./configs/vpn-check.sh);
-
-  # Waybar widget to show SSH and Kerberos state
-  programs.waybar.settings.mainBar."custom/access".exec =
-    (pkgs.writeShellScriptBin "access" (builtins.readFile ./configs/access.sh)) + "/bin/access";
-
   # Auto-run applications
   wayland.windowManager.sway.config = {
     startup = [
-      { command = "swaymsg 'workspace 1; exec firefox'"; }
-      { command = "swaymsg 'workspace 2; exec kitty'"; }
-      { command = "swaymsg 'exec \"sleep 1; flameshot\"'"; }
+      {command = "swaymsg 'workspace 1; exec firefox'";}
+      {command = "swaymsg 'workspace 2; exec kitty'";}
+      {command = "swaymsg 'exec \"sleep 1; flameshot\"'";}
     ];
   };
 
   # Waybar widgets position and battery
-  programs.waybar.settings.mainBar = {
-    modules-right = lib.mkForce [
-      "custom/access"
-      "battery"
-      "pulseaudio"
-      "network"
-      "custom/vpn"
-      "sway/language"
-      "custom/suspend"
-      "tray"
-    ];
 
-    "battery" = {
-      bat = "BAT0";
-      interval = 60;
-      states = {
-        warning = 30;
-        critical = 15;
-      };
-      format = "{capacity}% {icon}";
-      #format-icons = {
+  programs.waybar = {
+    settings.mainBar = {
+      # Waybar widget to show work VPN connectivity
+      "custom/vpn".exec =
+        builtins.readFile ./configs/vpn-check.sh;
+      # Waybar widget to show SSH and Kerberos state
+      "custom/access".exec =
+        (pkgs.writeShellScriptBin "access" (builtins.readFile ./configs/access.sh)) + "/bin/access";
+
+      modules-right = lib.mkForce [
+        "custom/access"
+        "battery"
+        "pulseaudio"
+        "network"
+        "custom/vpn"
+        "sway/language"
+        "custom/suspend"
+        "tray"
+      ];
+
+      "battery" = {
+        bat = "BAT0";
+        interval = 60;
+        states = {
+          warning = 30;
+          critical = 15;
+        };
+        format = "{capacity}% {icon}";
+        #format-icons = {
         #default = ["", "", "", "", "", "", ""];
-      #};
-      max-length = 25;
+        #};
+        max-length = 25;
+      };
     };
   };
 }
