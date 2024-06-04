889541ef5b36301394bb939ecd722345a53e2a92
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2024-06-04 20:51:21 +0200

    neovim: fix height of tree-sitter context (function headers)

--- configs/neovim/init.lua
+++ configs/neovim/init.lua
@@ -278,3 +278,7 @@ vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document
 vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
 vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
 vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+
+require('treesitter-context').setup{
+  max_lines = 2,
+}
