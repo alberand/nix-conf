8fdd7bbba02f3b4fc6b63173de59afbde7df84a6
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2024-06-11 13:40:11 +0200

    kanshi: add dual-monitor mode

--- modules/kanshi.nix
+++ modules/kanshi.nix
@@ -23,6 +23,23 @@
       }
 
       {
+        profile.name = "dual-monitor";
+        profile.outputs = [
+          {
+            criteria = "Dell Inc. DELL P2414H KKMMW6451WCS";
+            mode = "1920x1080@60Hz";
+            position = "0,0";
+          }
+          {
+            criteria = "Hewlett Packard HP E231 3CQ4231P8R";
+            mode = "1920x1080@60Hz";
+            transform = "90";
+            position = "-1080,-600";
+          }
+        ];
+      }
+
+      {
         profile.name = "home";
         profile.outputs = [
           {
