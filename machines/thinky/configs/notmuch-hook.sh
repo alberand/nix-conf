5ab208198eae37c6af945481b33655c7de410dad
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2024-08-30 10:21:02 +0200

    neomutt: autoread Jira emails from bot

--- machines/thinky/configs/notmuch-hook.sh
+++ machines/thinky/configs/notmuch-hook.sh
@@ -33,6 +33,8 @@ notmuch tag +bot +bugzilla -inbox -new -- from:bugzilla-daemon@kernel.org
 notmuch tag +bot +bugzilla -inbox -new -- to:bugzilla@redhat.com
 notmuch tag +bot +bugzilla -inbox -new -- to:bugzilla-daemon@kernel.org
 notmuch tag +bot +bugzilla -inbox -new -- from:jira-issues@redhat.com
+notmuch tag -unread -- from:jira-issues@redhat.com and from:"Errata Tool (Jira)"
+notmuch tag -unread -- from:jira-issues@redhat.com and from:"Gitlab-jira-bot Gitlab-redhat (Jira)"
 
 notmuch tag +bot +beaker -inbox -new -- subject:'\[Beaker*' or from:beaker@redhat.com or from:*@*rdu2.redhat.com or from:*@*bos.redhat.com
 notmuch tag +bot +brew -inbox -new -- from:brew-task-repos@redhat.com
