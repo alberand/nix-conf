2fb6fcdd581f555a64abe79b9ca8d05949f5a642
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2024-12-13 10:17:51 +0100

    nixxy: working tailscale + wireguard tunnel

--- modules/jellyfin-tunnel.nix
+++ modules/jellyfin-tunnel.nix
@@ -1,13 +1,13 @@
 {...}: {
   networking.wg-quick.interfaces = {
     wg0 = {
-      address = ["10.10.10.2/32"];
+      address = ["10.10.10.2"];
       privateKeyFile = "/etc/jfwg/client.private";
 
       peers = [
         {
           publicKey = "MKrNqXfz4sMtRekE44eHLdS/epD0MRZDd/PslJilr1A=";
-          allowedIPs = ["0.0.0.0/0"];
+          allowedIPs = ["10.10.10.0/30"];
           endpoint = "89.221.212.102:51820";
           persistentKeepalive = 25;
         }
--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -4,12 +4,11 @@
     port = 41641;
     openFirewall = true;
   };
-  networking.nameservers = [ "100.100.100.100" "8.8.8.8" "1.1.1.1" ];
-  networking.search = [ "tail5708e.ts.net" ];
-
-  #networking.interfaces.enp34s0.ipv4.routes = [{
-  #address = "185.195.233.66";
-  #prefixLength = 32;
-  #via = "192.168.0.1";
-  #}];
+  networking = {
+    nameservers = ["8.8.8.8" "1.1.1.1"];
+    firewall = {
+      checkReversePath = "loose";
+      trustedInterfaces = ["tailscale0"];
+    };
+  };
 }
