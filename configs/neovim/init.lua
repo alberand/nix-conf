a97dea52ed8a167d8d28d76b4e035a2e78632bea
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2024-12-18 18:06:49 +0100

    neovim: used default (longer) timeout for leader

--- configs/neovim/init.lua
+++ configs/neovim/init.lua
@@ -47,10 +47,6 @@ vim.opt.signcolumn = 'yes'
 -- Decrease update time
 vim.opt.updatetime = 250
 
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
-
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
