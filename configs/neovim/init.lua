9f020417a5383be8bb51619dd6cf3aec8acb6be2
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2024-12-20 15:35:35 +0100

    neovim: add key mapping to insert Reviewed-by tag

--- configs/neovim/init.lua
+++ configs/neovim/init.lua
@@ -163,6 +163,9 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
+-- Review tag for Kernel work
+vim.keymap.set('i', '<C-s>', 'Reviewed-by: Andrey Albershteyn <aalbersh@kernel.org>')
+
 -- Plugins
 
 -- Status line
