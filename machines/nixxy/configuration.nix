3f1ddb6da627c95df4835ebdbd831e3ae3a1e4b1
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2025-03-27 12:25:26 +0100

    nixxy: trust nixremote builder user

--- machines/nixxy/configuration.nix
+++ machines/nixxy/configuration.nix
@@ -280,6 +280,8 @@
       server = {port = 6742;};
     };
 
+    nix.settings.trusted-users = ["nixremote"];
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
