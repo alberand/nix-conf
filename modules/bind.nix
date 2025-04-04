025c2682ba3520a98b1b23d5ae754e40acb6617f
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2025-04-04 12:04:52 +0200

    nixxy: redo DNS setup for intranet

--- modules/bind.nix
+++ modules/bind.nix
@@ -1,35 +1,45 @@
 {pkgs, ...}: {
   services.bind = {
     enable = true;
+    forwarders = [
+      "1.1.1.1"
+      "8.8.8.8"
+      "8.8.4.4"
+    ];
     extraOptions = ''
       dnssec-validation auto;
       allow-query-cache { any; };
     '';
-    zones = {
-      "lan" = {
+    zones = let
+      fqdn = "alberand.com";
+      mainserver = "100.69.0.100";
+    in {
+      "${fqdn}" = {
+        allowQuery = [
+          "localhost"
+          "100.69.0.0/24"
+        ];
         master = true;
-        file = let
-          #mainserver = "192.168.0.100";
-          mainserver = "100.69.0.100";
-        in
-          pkgs.writeText "lan" ''
-            $ORIGIN lan.
-            $TTL    604800
-            lan.          IN      SOA     home.lan. root.home.lan. (
-                                         69         ; Serial
-                                         4h         ; refresh
-                                        15m         ; retry
-                                         8h         ; expire
-                                     604800 )       ; Negative Cache TTL
-                       IN      NS      home
-                       IN      A       ${mainserver}
-            ns.home    IN      A       ${mainserver}
-            home       IN      A       ${mainserver}
-            photos     IN      A       ${mainserver}
-            movies     IN      A       ${mainserver}
-            food       IN      A       ${mainserver}
-            git        IN      A       ${mainserver}
-          '';
+        file = pkgs.writeText "${fqdn}" ''
+          $ORIGIN ${fqdn}.
+          $TTL    604800
+          @              IN      SOA     ns admin (
+                                       69         ; Serial
+                                       4h         ; refresh
+                                      15m         ; retry
+                                       8h         ; expire
+                                   604800         ; Negative Cache TTL
+                                   )
+          @             IN      NS      ns
+          ns            IN      A       ${mainserver}
+
+          home          IN      A       ${mainserver}
+          photos        IN      A       ${mainserver}
+          movies        IN      A       ${mainserver}
+          food          IN      A       ${mainserver}
+          git           IN      A       ${mainserver}
+          jellyfin      IN      A       89.221.212.102
+        '';
       };
     };
   };
--- modules/caddy.nix
+++ modules/caddy.nix
@@ -5,11 +5,30 @@
 }: {
   age.secrets.acme-env.file = ../secrets/acme-env.age;
 
-  security.acme = {
+  security.acme = let
+    certfor = {name}: {
+      group = config.services.caddy.group;
+
+      domain = "${name}.alberand.com";
+      dnsProvider = "wedos";
+      dnsResolver = "ns.wedos.net:53";
+      dnsPropagationCheck = true;
+      enableDebugLogs = true;
+      environmentFile = config.age.secrets.acme-env.path;
+    };
+  in {
     acceptTerms = true;
     defaults.email = "andrey.albershteyn@gmail.com";
     defaults.enableDebugLogs = true;
     #defaults.server = "https://acme-staging-v02.api.letsencrypt.org/directory";
+
+    certs = {
+      "home.alberand.com" = certfor { name = "home"; };
+      "git.alberand.com" = certfor { name = "git"; };
+      "food.alberand.com" = certfor { name = "food"; };
+      "photos.alberand.com" = certfor { name = "photos"; };
+      "movies.alberand.com" = certfor { name = "movies"; };
+    };
 
     certs."whereisiss.com" = {
       group = config.services.caddy.group;
@@ -27,16 +46,7 @@
       enableDebugLogs = true;
       environmentFile = config.age.secrets.acme-env.path;
     };
-    certs."git.alberand.com" = {
-      group = config.services.caddy.group;
 
-      domain = "git.alberand.com";
-      dnsProvider = "wedos";
-      dnsResolver = "ns.wedos.net:53";
-      dnsPropagationCheck = true;
-      enableDebugLogs = true;
-      environmentFile = config.age.secrets.acme-env.path;
-    };
     certs."test.nemambyt.com" = {
       group = config.services.caddy.group;
 
@@ -66,75 +76,49 @@
         '';
       };
       certloc = "/var/lib/acme/whereisiss.com";
-      gitcertloc = "/var/lib/acme/git.alberand.com";
+      certlocname = {name}: "/var/lib/acme/${name}.alberand.com";
       nbcertloc = "/var/lib/acme/test.nemambyt.com";
     in {
-      "home.lan".extraConfig = ''
-        tls internal
+      "home.alberand.com".extraConfig = ''
         encode gzip
         root * ${dashboard}/
         file_server
-      '';
-      "movies.lan".extraConfig = ''
-        tls internal
-        encode gzip
-        reverse_proxy 127.0.0.1:55686
-      '';
-      "photos.lan".extraConfig = ''
-        tls internal
-        encode gzip
-        reverse_proxy 127.0.0.1:8113
-      '';
-      "food.lan".extraConfig = ''
-        tls internal
-        encode gzip
-        reverse_proxy 127.0.0.1:8114
-      '';
-      "git.lan".extraConfig = ''
-        tls internal
-        encode gzip
-        reverse_proxy 127.0.0.1:3000
-      '';
-      "home.whereisiss.com".extraConfig = ''
-        encode gzip
-        root * ${dashboard}/
-        file_server
-        tls ${certloc}/cert.pem ${certloc}/key.pem {
+        tls ${certlocname {name = "home";}}/cert.pem ${certlocname {name = "home";}}/key.pem {
           protocols tls1.3
         }
       '';
-      "movies.whereisiss.com".extraConfig = ''
+      "movies.alberand.com".extraConfig = ''
+        encode gzip
+        reverse_proxy 127.0.0.1:55686
+        tls ${certlocname {name = "movies";}}/cert.pem ${certlocname {name = "movies";}}/key.pem {
+          protocols tls1.3
+        }
+      '';
+      "jellyfin.alberand.com".extraConfig = ''
         encode gzip
         reverse_proxy 127.0.0.1:55686
         tls ${certloc}/cert.pem ${certloc}/key.pem {
           protocols tls1.3
         }
       '';
-      "photos.whereisiss.com".extraConfig = ''
+      "photos.alberand.com".extraConfig = ''
         encode gzip
         reverse_proxy 127.0.0.1:8113
-        tls ${certloc}/cert.pem ${certloc}/key.pem {
+        tls ${certlocname {name = "photos";}}/cert.pem ${certlocname {name = "photos";}}/key.pem {
           protocols tls1.3
         }
       '';
-      "food.whereisiss.com".extraConfig = ''
+      "food.alberand.com".extraConfig = ''
         encode gzip
         reverse_proxy 127.0.0.1:8114
-        tls ${certloc}/cert.pem ${certloc}/key.pem {
-          protocols tls1.3
-        }
-      '';
-      "git.whereisiss.com".extraConfig = ''
-        encode gzip
-        reverse_proxy 127.0.0.1:3000
-        tls ${certloc}/cert.pem ${certloc}/key.pem {
+        tls ${certlocname {name = "food";}}/cert.pem ${certlocname {name = "food";}}/key.pem {
           protocols tls1.3
         }
       '';
       "git.alberand.com".extraConfig = ''
         encode gzip
         reverse_proxy 127.0.0.1:3000
-        tls ${gitcertloc}/cert.pem ${gitcertloc}/key.pem {
+        tls ${certlocname {name = "git";}}/cert.pem ${certlocname {name = "git";}}/key.pem {
           protocols tls1.3
         }
       '';
