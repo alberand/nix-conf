9715f81c6ee1c05d0490f07149c462b8eeda2656
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2025-05-14 10:20:37 +0200

    thinky: add more dual monitor modes to kanshi

--- modules/kanshi.nix
+++ modules/kanshi.nix
@@ -38,6 +38,38 @@
       }
 
       {
+        profile.name = "dual-monitor-v";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            mode = "1920x1080@60Hz";
+            position = "0,0";
+          }
+          {
+            criteria = "*";
+            mode = "1920x1080@60Hz";
+            position = "-200,1080";
+          }
+        ];
+      }
+
+      {
+        profile.name = "dual-monitor-h";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            mode = "1920x1080@60Hz";
+            position = "0,0";
+          }
+          {
+            criteria = "*";
+            mode = "1920x1080@60Hz";
+            position = "1920,0";
+          }
+        ];
+      }
+
+      {
         profile.name = "home";
         profile.outputs = [
           {
