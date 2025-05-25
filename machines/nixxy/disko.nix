074aaa4e245148f2ba37a99cc002f3019daf3d75
Andrey Albershteyn <aalbersh@kernel.org>
2025-05-25 13:03:51 +0200

    nixxy: add disko configuration (not tested)
    Signed-off-by: Andrey Albershteyn <aalbersh@kernel.org>

---
+++ machines/nixxy/disko.nix
@@ -0,0 +1,73 @@
+# sudo nix run 'github:nix-community/disko/latest#disko-install' -- --flake .#nixxy --disk <disk-name> <disk-device>
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/vdb";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "500M";
+              type = "EF00";
+              label = "boot";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                label = "luks";
+                extraOpenArgs = [];
+                settings = {
+                  # if you want to use the key for interactive login be sure there is no trailing newline
+                  # for example use `echo -n "password" > /tmp/secret.key`
+                  keyFile = "/tmp/secret.key";
+                  allowDiscards = true;
+                };
+                additionalKeyFiles = ["/tmp/additionalSecret.key"];
+                content = {
+                  type = "lvm_pv";
+                  vg = "pool";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          swap = {
+            size = "8G";
+            content = {
+              type = "swap";
+              randomEncryption = true;
+              priority = 100; # prefer to encrypt as long as we have space for it
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "xfs";
+              mountpoint = "/";
+              mountOptions = [
+                "defaults"
+              ];
+            };
+          };
+        };
+      };
+    };
+  };
+}
