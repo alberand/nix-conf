92d90fe7f0cfa9548fdde65db47b61a542aa64bd
Andrey Albershteyn <andrey.albershteyn@gmail.com>
2025-06-01 14:28:45 +0200

    nixxy: create a bridge for libvirt VMs
    Signed-off-by: Andrey Albershteyn <aalbersh@kernel.org>

--- machines/nixxy/configuration.nix
+++ machines/nixxy/configuration.nix
@@ -91,6 +91,17 @@
     networking.nat.internalInterfaces = ["ve-+"];
     networking.nat.externalInterface = "wlo1";
     networking.networkmanager.unmanaged = ["interface-name:ve-*"];
+    networking.bridges = {
+      "virbr0" = {
+        interfaces = ["enp34s0"];
+      };
+    };
+    networking.interfaces.virbr0.ipv4.addresses = [
+      {
+        address = "10.10.10.10";
+        prefixLength = 24;
+      }
+    ];
 
     # ash drive
     services.udev.extraRules = ''
