25a832ce3e594df422e9cfb8311d33a93f22664d
Andrey Albershteyn <aalbersh@kernel.org>
2025-06-06 14:15:08 +0200

    nixxy: remove redundant networking configuration options
    Signed-off-by: Andrey Albershteyn <aalbersh@kernel.org>

--- machines/nixxy/configuration.nix
+++ machines/nixxy/configuration.nix
@@ -62,8 +62,6 @@
       # Pick only one of the below networking options.
       networkmanager.enable = true;
       networkmanager.dns = "default";
-      #defaultGateway = "192.168.0.1";
-      interfaces.enp34s0.useDHCP = true;
       firewall.enable = true;
       firewall = {
         # Syncthing opens ports by itself
@@ -91,17 +89,6 @@
     networking.nat.internalInterfaces = ["ve-+"];
     networking.nat.externalInterface = "wlo1";
     networking.networkmanager.unmanaged = ["interface-name:ve-*"];
-    networking.bridges = {
-      "virbr0" = {
-        interfaces = ["enp34s0"];
-      };
-    };
-    networking.interfaces.virbr0.ipv4.addresses = [
-      {
-        address = "10.10.10.10";
-        prefixLength = 24;
-      }
-    ];
 
     # ash drive
     services.udev.extraRules = ''
