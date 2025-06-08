daf9d3f63dc8e650efc05fa6bae7e5f506324113
Andrey Albershteyn <aalbersh@kernel.org>
2025-06-08 13:46:58 +0200

    nixxy: connect podman-deluge container to 'cnet' network
    Signed-off-by: Andrey Albershteyn <aalbersh@kernel.org>

--- modules/jellyfin.nix
+++ modules/jellyfin.nix
@@ -203,24 +203,33 @@
     "deluge" = {
       image = "binhex/arch-delugevpn";
       autoStart = true;
+      networks = ["cnet"];
       ports = ["8112:8112" "8118:8118" "58846:58846" "58946:58946"];
+
+      extraOptions = [
+        "--ip=10.10.10.50"
+        "--privileged=true"
+        ''--sysctl="net.ipv4.conf.all.src_valid_mark=1"''
+        ''--sysctl="net.ipv4.ip_forward=1"''
+      ];
 
       volumes = [
         "/media:/media"
-        "/home/alberand/.deluge:/config"
+        "/media/var/lib/deluge:/config"
         "/etc/localtime:/etc/localtime:ro"
       ];
 
       environment = {
-        PUID = "0";
-        PGID = "8096";
+        PUID = toString config.users.users.media.uid;
+        PGID = toString config.users.groups.media.gid;
         VPN_ENABLED = "yes";
         VPN_CLIENT = "wireguard";
         VPN_PROV = "custom";
         STRICT_PORT_FORWARD = "yes";
         ENABLE_PRIVOXY = "yes";
-        LAN_NETWORK = "10.10.10.100/32";
-        NAME_SERVERS = "84.200.69.80,37.235.1.174,1.1.1.1,37.235.1.177,84.200.70.40,1.0.0.1";
+        LAN_NETWORK = "10.10.10.0/24";
+        # DNS Watch #1 #3 Cloudflare #2 #4
+        NAME_SERVERS = "84.200.69.80,1.1.1.1,84.200.70.40,1.0.0.1";
         DELUGE_DAEMON_LOG_LEVEL = "trace";
         DELUGE_WEB_LOG_LEVEL = "trace";
         DEBUG = "true";
@@ -228,12 +237,6 @@
         TZ = "Europe/London";
         DELUGE_ENABLE_WEBUI_PASSWORD = "no";
       };
-
-      extraOptions = [
-        "--privileged=true"
-        ''--sysctl="net.ipv4.conf.all.src_valid_mark=1"''
-        ''--sysctl="net.ipv4.ip_forward=1"''
-      ];
     };
   };
 }
