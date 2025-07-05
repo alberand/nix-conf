4332fc83db9aad3d307a9bf9ba9a7a69a18a3dce
Andrey Albershteyn <aalbersh@kernel.org>
2025-07-05 13:33:13 +0200

    quesada: so far this host has only one ed25519 key
    Signed-off-by: Andrey Albershteyn <aalbersh@kernel.org>

--- machines/quesada/configuration.nix
+++ machines/quesada/configuration.nix
@@ -103,6 +103,12 @@
       PermitRootLogin = "no";
       PasswordAuthentication = false;
     };
+    hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
   };
 
   programs.zsh.enable = true;
