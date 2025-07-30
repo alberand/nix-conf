672df737f74b1cc704531d3fcb22ecdda8973ddb
Andrey Albershteyn <aalbersh@kernel.org>
2025-07-30 19:47:22 +0200

    nixxy: update VPN configuraiton
    Signed-off-by: Andrey Albershteyn <aalbersh@kernel.org>

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -13,29 +13,15 @@
     allowedUDPPorts = [config.networking.wg-quick.interfaces.wg0.listenPort];
   };
 
-  nixpkgs.overlays = [
-    (self: super: {
-      wireguard-tools = super.wireguard-tools.overrideAttrs rec {
-        version = "1.0.20250521";
-        src = pkgs.fetchzip {
-          url = "https://git.zx2c4.com/wireguard-tools/snapshot/wireguard-tools-${version}.tar.xz";
-          sha256 = "sha256-V9yKf4ZvxpOoVCFkFk18+130YBMhyeMt0641tn0O0e0=";
-        };
-        patches = [
-          ../patches/0001-wg-quick-linux-set-priority-of-the-ip-rule-higher-th.patch
-        ];
-      };
-    })
-  ];
-
   # Enable WireGuard
   networking.wg-quick.interfaces = let
-    server_ip = "185.195.233.66";
+    server_ip = "193.32.127.68";
   in {
     wg0 = {
-      address = ["10.64.156.60/32" "fc00:bbbb:bbbb:bb01::1:9c3b/128"];
+      autostart = false;
+      address = ["10.70.12.188/32" "fc00:bbbb:bbbb:bb01::7:cbb/128"];
 
-      dns = ["100.64.0.23"];
+      dns = ["100.64.0.55"];
 
       # to match firewall allowedUDPPorts (without this wg
       # uses random port numbers)
@@ -46,7 +32,7 @@
 
       peers = [
         {
-          publicKey = "1493vtFUbIfSpQKRBki/1d0YgWIQwMV4AQAvGxjCNVM=";
+          publicKey = "5Ms10UxGjCSzwImTrvEjcygsWY8AfMIdYyRvgFuTqH8=";
           allowedIPs = ["0.0.0.0/0" "::/0"];
           endpoint = "${server_ip}:51820";
           persistentKeepalive = 25;
