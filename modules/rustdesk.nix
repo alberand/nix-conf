a9860fb2e8b8c3ba4f221ea26290c2ef727f2a5c
Andrey Albershteyn <aalbersh@kernel.org>
2025-08-03 18:29:41 +0200

    nixxy: update rustdesk configuration
    Signed-off-by: Andrey Albershteyn <aalbersh@kernel.org>

--- modules/rustdesk.nix
+++ modules/rustdesk.nix
@@ -1,4 +1,34 @@
-{config, pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  networking.firewall = {
+    enable = true;
+    interfaces.headscale.allowedTCPPorts = [
+      21115
+      21116
+      21118
+      # relay
+      21117
+      21119
+    ];
+    interfaces.headscale.allowedUDPPorts = [
+      21116
+    ];
+    interfaces.wlo1.allowedTCPPorts = [
+      21115
+      21116
+      21118
+      # relay
+      21117
+      21119
+    ];
+    interfaces.wlo1.allowedUDPPorts = [
+      21116
+    ];
+  };
+
   environment.systemPackages = with pkgs; [
     rustdesk-flutter
   ];
@@ -8,6 +38,9 @@
     openFirewall = true;
     signal.enable = true;
     relay.enable = true;
-    signal.relayHosts = ["100.69.0.4"];
+    signal.relayHosts = [
+      "100.69.0.2"
+      "192.168.0.100"
+    ];
   };
 }
