9e66cb699c131f94a48d281d1f4349c0a231be1a
Andrey Albershteyn <aalbersh@kernel.org>
2025-09-23 10:56:23 +0200

    common: enable XDG

--- modules/home-base.nix
+++ modules/home-base.nix
@@ -95,6 +95,8 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  xdg.enable = true;
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
