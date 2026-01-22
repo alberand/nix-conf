b9493a1a6eee2ab5cc6277e39273856b997ece74
Andrey Albershteyn <aalbersh@kernel.org>
2026-01-22 15:51:00 +0100

    thinky: don't suspend when on power and lid is closed

--- machines/thinky/configuration.nix
+++ machines/thinky/configuration.nix
@@ -158,6 +158,10 @@
       };
     };
 
+    services.logind.settings.Login = {
+      HandleLidSwitchExternalPower = "ignore";
+    };
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
