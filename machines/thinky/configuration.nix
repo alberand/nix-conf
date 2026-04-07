3e3adf68ee9aea40fdb8fe285271cbbdab422d2f
Andrey Albershteyn <aalbersh@kernel.org>
2026-04-07 11:14:59 +0200

    thinky: minor formatting issue in configuration.nix

--- machines/thinky/configuration.nix
+++ machines/thinky/configuration.nix
@@ -76,7 +76,10 @@
       enable = true;
       dnssec = "true";
       domains = ["~."];
-      fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1"];
+      fallbackDns = [
+        "1.1.1.1#one.one.one.one"
+        "1.0.0.1"
+      ];
       extraConfig = ''
         DNSOverTLS=no
       '';
