cf5063f878cf42b33375d8df3400e2cc73e18669
Andrey Albershteyn <aalbersh@kernel.org>
2026-04-08 12:26:09 +0200

    neovim: higlight lines over 80 chars in C code

--- configs/ftplugin/c.lua
+++ configs/ftplugin/c.lua
@@ -2,3 +2,12 @@ vim.bo.tabstop = 8 -- size of a hard tabstop (ts).
 vim.bo.shiftwidth = 8 -- size of an indentation (sw).
 vim.bo.expandtab = false -- always uses spaces instead of tab characters (et).
 vim.bo.softtabstop = 8 -- number of spaces a <Tab> counts for. When 0, feature is o
+
+-- Highlight lines longer than textwidth
+vim.api.nvim_set_hl(0, 'OverLength', { bg = 'red' })
+vim.api.nvim_create_autocmd({ 'BufWinEnter', 'BufRead', 'BufNewFile' }, {
+  pattern = '*',
+  callback = function()
+    vim.fn.matchadd('OverLength', '\\%81v.*\\S')
+  end,
+})
