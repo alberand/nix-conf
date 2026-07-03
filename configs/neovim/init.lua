d123391a302fdaabf50a5174de9dee34329096c0
Andrey Albershteyn <aalbersh@kernel.org>
2026-07-03 12:21:37 +0200

    neovim: remove deprecated function

--- configs/neovim/init.lua
+++ configs/neovim/init.lua
@@ -111,8 +111,7 @@ end
 
 -- Disable LSP log as it can grew pretty big
 -- When needed enable with
--- vim.lsp.set_log_level("debug")
-vim.lsp.set_log_level("off")
+vim.lsp.log.set_level("off")
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
